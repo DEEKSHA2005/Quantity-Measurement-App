e43ddd4d137349fca07e0aa87399df53f881bced
Deeksha <smdeeksha2005@gmail.com>
2026-02-25 15:16:13 +0530

    [Deeksha]: UC4: Extended unit support (Yards & Centimeters)

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -57,8 +57,11 @@ public class QuantityMeasurementApp {
         private final LengthUnit unit;
 
         public enum LengthUnit {
-            FEET(12.0),      // 1 foot = 12 inches
-            INCHES(1.0);
+
+            FEET(12.0),          // 1 foot = 12 inches
+            INCHES(1.0),        // base unit
+            YARDS(36.0),        // 1 yard = 36 inches
+            CENTIMETERS(0.393701); // 1 cm = 0.393701 inches
 
             private final double conversionFactor;
 
